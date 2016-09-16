2bf47dd86008d9c5862013fbbca4d3e96d353425
Milica Veljkovic <m.veljkovic@levi9.com>
2016-09-16 14:51:36 +0200

    konflikt u pokusaju

--- prviFajl.js
+++ prviFajl.js
@@ -1,6 +1,4 @@
-var a = 0;
-var b = 2;
-var c = a+b;
-console.log (c, 'rezultati');
-console.log ('promene na zivo');
-console.log ('druga grana');
+console.log('hello world');
+var b = 'test';
+
+a = b+c;
